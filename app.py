606766ca2e32288a2faf3088c88156bd12174b6c
Alissa <alissatroianonyc@gmail.com>
2023-04-15 22:28:07 -0400

    Uncomment relevant code for teammate review

--- app.py
+++ app.py
@@ -18,23 +18,23 @@ def index():
     return 'Connected to MindsDB server. The BTC model is: {}'.format(model.name)
 
 
-# @app.route('/bitcoin', methods=['GET', 'POST'])
-# def bitcoin():
-#     form = BitcoinForm()
-#     if form.validate_on_submit():
-#         try:
-#             input_data = {'coin': form.coin.data}
-#             project = server.get_project('mindsdb')
-#             model = project.get_model('crypto4')
-#             prediction = model.predict(input_data)
-#             target = prediction[0]['close_price']
-#             lower_bound = prediction[0]['close_price_confidence']['lower_bound']
-#             upper_bound = prediction[0]['close_price_confidence']['upper_bound']
-#             return render_template('bitcoin.html', target=target, lower_bound=lower_bound, upper_bound=upper_bound, form=form)
-#         except Exception as e:
-#             print(str(e))
-#             return render_template('500.html'), 500
-#     return render_template('bitcoin.html', form=form)
+@app.route('/bitcoin', methods=['GET', 'POST'])
+def bitcoin():
+    form = BitcoinForm()
+    if form.validate_on_submit():
+        try:
+            input_data = {'coin': form.coin.data}
+            project = server.get_project('mindsdb')
+            model = project.get_model('crypto4')
+            prediction = model.predict(input_data)
+            target = prediction[0]['close_price']
+            lower_bound = prediction[0]['close_price_confidence']['lower_bound']
+            upper_bound = prediction[0]['close_price_confidence']['upper_bound']
+            return render_template('bitcoin.html', target=target, lower_bound=lower_bound, upper_bound=upper_bound, form=form)
+        except Exception as e:
+            print(str(e))
+            return render_template('500.html'), 500
+    return render_template('bitcoin.html', form=form)
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
