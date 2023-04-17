3c6d3db344719f0705e992eb9654972bc9fa0ee7
Alissa <alissatroianonyc@gmail.com>
2023-04-17 10:39:47 -0400

    Remove commented out code

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 
 app.config.from_object(Config)
 
+server = mindsdb_sdk.connect('https://cloud.mindsdb.com', email=MINDSDB_EMAIL, password=MINDSDB_PASSWORD)
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -32,21 +34,6 @@ def bitcoin():
         return 'Your prediction data for Bitcoin is: ' + str(query.fetch())
     return render_template('bitcoin.html', form=form, query=None)
 # run `flask run``, then visit 127.0.0.1:5000/bitcoin/ to test this function
-
-
-# @app.route('/ethereum', methods=['GET', 'POST'])
-# def ethereum():
-#     form = CoinForm()
-#     if form.validate_on_submit():
-#         # Use MindsDB server object here
-#         project = server.get_project('mindsdb')
-#         databases = server.list_databases()
-#         model = project.get_model('ethusd_prediction_mod')
-#         query = project.query('SELECT * FROM mindsdb.ethusd_prediction_mod WHERE date="2019-01-05"');
-#         close_price_prediction = query.fetch()
-#         # We need to strip the query.fetch() so the result is just the close_price. Right now this returns: 'Your prediction data for Bitcoin is: date ... close_price_max 0 2019-01-05 ... 3796.056272 [1 rows x 14 columns]'
-#         return 'Your prediction data for Ethereum is: ' + str(query.fetch())
-#     return render_template('ethereum.html', form=form, query=None)
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
