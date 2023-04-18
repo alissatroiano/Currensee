80c417295eb92bc211ebb3e77769126ccd28035b
Alissa <alissatroianonyc@gmail.com>
2023-04-18 17:18:31 -0400

    Create coin display page with clickable card

--- app.py
+++ app.py
@@ -20,10 +20,9 @@ def index():
     model = project.get_model('btcusd_predictor')
     return render_template('index.html', model=model)
     # run `flask run` command in terminal, then visit 127.0.0.1:5000 to test this function
-    
 
-@app.route('/bitcoin', methods=['GET', 'POST'])
-def bitcoin():
+@app.route('/coins', methods=['GET', 'POST'])
+def coins():
     form = CoinForm()
     if form.validate_on_submit():
         # Use MindsDB server object he`re
@@ -32,9 +31,23 @@ def bitcoin():
         print(model)
         query = project.query('SELECT close_price FROM mindsdb.btcusd_prediction_mod WHERE date="2019-01-05"');
         return 'Your prediction data for Bitcoin is: ' + str(query.fetch())
-    return render_template('bitcoin.html', form=form, query=None)
+    return render_template('coins.html', form=form, query=None)
+
+@app.route('/bitcoin', methods=['GET', 'POST'])
+def bitcoin():
+    """
+    Method to return bitcoin prediction data when a user clicks the bitcoin card on coins.html
+    """
+    project = server.get_project('mindsdb')
+    model = project.get_model('btcusd_prediction_mod')
+    print(model)
+    query = project.query('SELECT close_price FROM mindsdb.btcusd_prediction_mod WHERE date="2019-01-05"');
+
+    return 'Your prediction data for Bitcoin is: ' + str(query.fetch())
+
 # run `flask run``, then visit 127.0.0.1:5000/bitcoin/ to test this function
 
+
+
 if __name__ == '__main__':
-    # set debug to true
     app.run(debug=True)
