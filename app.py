722a436665d7c674d63c078ba8ee3ed8929e783f
Alissa <alissatroianonyc@gmail.com>
2023-04-26 15:42:02 -0400

    Update .gitignore with settings.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
 import ssl
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import mindsdb_sdk 
 from config import Config
 from settings import MINDSDB_EMAIL, MINDSDB_PASSWORD
 from forms import CoinForm
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain('server.crt', 'server.key')
+# context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+# context.load_cert_chain('server.crt', 'server.key')
 
 app = Flask(__name__)
 
@@ -14,23 +14,14 @@ app.config.from_object(Config)
 
 server = mindsdb_sdk.connect('https://cloud.mindsdb.com', email=MINDSDB_EMAIL, password=MINDSDB_PASSWORD)
 
-
 @app.route('/')
 def index():
     return render_template('index.html', title='Home')
 
 
-@app.route('/coins', methods=['GET', 'POST'])
+@app.route('/coins')
 def coins():
-    form = CoinForm()
-        # Use MindsDB server object he`re
-    project = server.get_project('mindsdb')
-    model = project.get_model('btcusd_prediction_mod')
-    print(model)
-    query = project.query('SELECT close_price FROM mindsdb.btcusd_prediction_mod WHERE date="2023-04-25"')
-    print(str(query.fetch()))
-
-    return render_template('coins.html', form=form, query=None)
+    return render_template('coins.html', title='Coins')
 
 
 @app.route('/bitcoin', methods=['GET', 'POST'])
@@ -38,15 +29,18 @@ def bitcoin():
     """
     Method to return bitcoin prediction data when a user clicks the bitcoin card on coins.html
     """
-    project = server.get_project('mindsdb')
-    model = project.get_model('btcusd_prediction_mod')
-    print(model)
-    query = project.query('SELECT close_price FROM mindsdb.btcusd_prediction_mod WHERE date="2023-04-25"')
-    print(str(query.fetch()))
+    # if request.method == 'POST':
+    #     server = mindsdb_sdk.connect('https://cloud.mindsdb.com', email=MINDSDB_EMAIL, password=MINDSDB_PASSWORD)
+
+    #     databases = server.list_databases()
+    #     print(databases)
+    # model = project.get_model('btcusd_prediction_mod')
+    # print(model)
+    # query = project.query('SELECT close_price FROM mindsdb.btcusd_prediction_mod WHERE date="2019-04-25"')
+    # print(str(query.fetch()))
     # result = model.query.with_entities('close_price'.column_name).fetch()
 
-    return render_template('bitcoin.html', query=query)
-# run `flask run``, then visit 127.0.0.1:5000/bitcoin/ to test this function
+    return render_template('bitcoin.html')
 
 @app.route('/ethereum', methods=['GET', 'POST'])
 def ethereum():
@@ -54,7 +48,7 @@ def ethereum():
     Method to return ethereum prediction data when a user clicks the ethereum card on coins.html
     """
     # project = server.get_project('mindsdb')
-    # database = server.get_database('files')
+    # databases = server.get_database('files')
     # model = project.get_model('ethereum_predictions')
     # query = project.query('SELECT * FROM ethereum_predictions as EP JOIN files.Ethereum as E WHERE E.Date > "2023-04-25" LIMIT 1;')
     # print(query.fetch())
@@ -62,4 +56,4 @@ def ethereum():
 
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, ssl_context=context)
+   app.run('0.0.0.0', port=5000)
--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 MINDSDB_EMAIL = 'alissatroianonyc@gmail.com'
 MINDSDB_PASSWORD = 'B@nana$5900'
 SECRET_KEY = 'IP5)xqI,U&GqNcz[I-o+@XY3WVq2n>'
-DEBUG = 'True'
+DEBUG = 'False'
