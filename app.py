e9c72244ea3e4bb78e1d5fb671e58bfe0138f301
Alissa <alissatroianonyc@gmail.com>
2023-04-28 17:20:47 -0400

    Update data, add image for favicon

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from config import Config
 from settings import MINDSDB_EMAIL, MINDSDB_PASSWORD
 from forms import CoinForm
 import pandas as pd
+# import numpy as np # linear algebra
 
 
 app = Flask(__name__)
@@ -42,6 +43,11 @@ def ethereum():
     # project = server.get_project('mindsdb')
     # databases = server.get_database('files')
     # model = project.get_model('ethereum_predictions')
+    df = pd.read_csv('bitcoin.csv')
+    # print(df)
+    header = df.head()
+    print(header)
+
     # query = project.query('SELECT EP.Close, Date FROM ethereum_predictions as EP JOIN files.Ethereum as E WHERE E.Date > "2023-04-26" LIMIT 1;')
     # print(query.fetch())
     return render_template('ethereum.html')
---
+++ queries.md
@@ -0,0 +1,9 @@
+-- CREATE MODEL bitcoin
+-- FROM files (
+--   SELECT * FROM bitcoin
+--   )
+-- PREDICT Close
+-- ORDER BY Date
+-- WINDOW 3125
+-- HORIZON 7;
+
