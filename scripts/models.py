5e1cb8bf04696b8b5a8b97c5277888f7629ed635
Alissa <alissatroianonyc@gmail.com>
2023-04-29 15:05:53 -0400

    Update cover page and coin layout

--- scripts/models.py
+++ scripts/models.py
@@ -3,7 +3,6 @@ MindsDB Model Query Scripts Will Load/Be Defined Here
 """
 from config import Config
 from settings import MINDSDB_EMAIL, MINDSDB_PASSWORD
-from forms import CoinForm
 import pandas as pd
 import mindsdb_sdk 
 
