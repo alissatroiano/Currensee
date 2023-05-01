716788c84b7799d123479e91b9883e1d1faa1032
rohan472000 <anand00rohan@gmail.com>
2023-05-01 13:09:18 +0530

    updated syntax errors with ruff

--- scripts/models.py
+++ scripts/models.py
@@ -1,9 +1,5 @@
 """
 MindsDB Model Query Scripts Will Load/Be Defined Here
 """
-from config import Config
-from settings import MINDSDB_EMAIL, MINDSDB_PASSWORD
-import pandas as pd
-import mindsdb_sdk 
 
 
