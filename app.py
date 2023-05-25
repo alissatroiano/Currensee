62b4fbb5c425e87143398c6513c2f4c36164f5e4
Alissa <alissatroianonyc@gmail.com>
2023-05-25 01:31:45 -0400

    Add new bitcoin model

--- app.py
+++ app.py
@@ -3,52 +3,33 @@ from flask import Flask, render_template, request
 import mindsdb_sdk 
 from config import Config
 from settings import MINDSDB_EMAIL, MINDSDB_PASSWORD
+
 from forms import CoinForm, RegisterForm, LoginForm
 import pandas as pd
 from pandas import DataFrame
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_bcrypt import Bcrypt
+# from flask_migrate import Migrate
+# from flask_login import LoginManager
+# from models import User
 
-server = mindsdb_sdk.connect('https://cloud.mindsdb.com', email=MINDSDB_EMAIL, password=MINDSDB_PASSWORD)
 
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_migrate import Migrate
-from flask_login import LoginManager
-from models import User
-
-# Flask Login
-from flask_login import (
-    UserMixin,
-    login_user,
-    LoginManager,
-    current_user,
-    logout_user,
-    login_required,
-)
-login_manager = LoginManager()
-login_manager.session_protection = "strong"
-login_manager.login_view = "login"
-login_manager.login_message_category = "info"
+# # Flask Login
+# from flask_login import (
+#     UserMixin,
+#     login_user,
+#     LoginManager,
+#     current_user,
+#     logout_user,
+#     login_required,
+# )
+# login_manager = LoginManager()
+# login_manager.session_protection = "strong"
+# login_manager.login_view = "login"
+# login_manager.login_message_category = "info"
 # import psycopg2
 basedir = os.path.abspath(os.path.dirname(__file__))
-from flask_sqlalchemy import SQLAlchemy
-
-
-def create_app():
-    app = Flask(__name__)
-    login_manager = LoginManager()
-    login_manager.login_view = 'login'
-
-    app.config.from_object(Config)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    login_manager.init_app(app)
-
-    server = mindsdb_sdk.connect('https://cloud.mindsdb.com', email=MINDSDB_EMAIL, password=MINDSDB_PASSWORD)
-    db = SQLAlchemy(app)
-    bcrypt = Bcrypt(app)
-    migrate = Migrate(app, db)
-
-    return app, db, bcrypt, migrate, server, login_manager
+server = mindsdb_sdk.connect('https://cloud.mindsdb.com', email=MINDSDB_EMAIL, password=MINDSDB_PASSWORD)
 
 app = Flask(__name__)
 
@@ -56,22 +37,22 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', title='Home')
 
-@login_manager.user_loader
-def user_loader(user_id):
-    """Given *user_id*, return the associated User object.
+# @login_manager.user_loader
+# def user_loader(user_id):
+#     """Given *user_id*, return the associated User object.
 
-    :param unicode user_id: user_id (email) user to retrieve
+#     :param unicode user_id: user_id (email) user to retrieve
 
-    """
-    return User.query.get(user_id)
+#     """
+#     return User.query.get(user_id)
 
-def validate_email(self, email):
-        if User.query.filter_by(email=email.data).first():
-            raise ValidationError("Email already registered!")
+# def validate_email(self, email):
+#         if User.query.filter_by(email=email.data).first():
+#             raise ValidationError("Email already registered!")
 
-def validate_uname(self, uname):
-    if User.query.filter_by(username=username.data).first():
-        raise ValidationError("Username already taken!")
+# def validate_uname(self, uname):
+#     if User.query.filter_by(username=username.data).first():
+#         raise ValidationError("Username already taken!")
 
 # @login_manager.user_loader
 # def load_user(user_id):
@@ -101,10 +82,10 @@ def bitcoin():
     Method to return bitcoin prediction data when a user clicks the bitcoin card on coins.html
     """
     project = server.get_project('mindsdb')
-    query = project.query('SELECT T.Date as Date, T.Close as Close FROM mindsdb.eth_1 as T JOIN files.Ethereum as P WHERE P.Date > LATEST LIMIT 14;')
+    query = project.query('SELECT T.date as date, T.price as Prediction, price_explain FROM mindsdb.bitcoin_model as T JOIN files.bitcoin_data as P WHERE P.Date > LATEST LIMIT 14')
     # create a dataframe for data from query
-    eth_df = DataFrame.to_html(query.fetch(), index=False)
-    return render_template('coins/bitcoin.html')
+    btc_df = DataFrame.to_html(query.fetch(), index=False)
+    return render_template('coins/bitcoin.html', query=query, btc_df=btc_df)
 
 @app.route('/ethereum', methods=['GET', 'POST'])
 def ethereum():
