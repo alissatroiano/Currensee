309d37964ac68a483fd888f0e0ef3fa3162f9149
Alissa <alissatroianonyc@gmail.com>
2023-05-25 02:13:55 -0400

    Update ethereum model to improve speed and make current predictions

--- app.py
+++ app.py
@@ -42,9 +42,7 @@ def ethereum():
     a user clicks the ethereum card on coins.html
     """
     project = server.get_project('mindsdb')
-    query = project.query('SELECT T.Date as Date, T.Close '
-                          'as Close, Close_Explain FROM mindsdb.eth_1 as T JOIN files.Ethereum '
-                          'as P WHERE P.Date > LATEST LIMIT 7;')
+    query = project.query('SELECT T.Date as Date, T.price AS Price, Price_Explain FROM mindsdb.ethereum as T JOIN files.ethereum_data as P WHERE P.Date > LATEST LIMIT 7;')
     # create a dataframe for data from query
     eth_df = DataFrame.to_html(query.fetch(), index=False)
 
